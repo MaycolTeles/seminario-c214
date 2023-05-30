f8444bd4564964c7dde300df8a99913f47a08c54
Maycol <maycolteles@hotmail.com>
2023-05-30 17:29:43 -0300

    build: adding a CI file to use github actions.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+
+with open('README.md') as file:
+    readme = file.read()
+
+
+setup(
+    name='calculator',
+    version='0.0.1',
+    packages=find_packages(where='app'),
+    description='A calculator package',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Maycol',
+    author_email='maycolteles@hotmail.com',
+    license='GNU',
+    install_requires=[],
+    extras_require={
+        'dev': ['pytest >= 7.3.1', 'pytest-cov >= 2.10.1', 'flake8 >= 3.8.4', 'twine >= 4.0.2'],
+    },
+    python_requires='>=3.9',
+)
